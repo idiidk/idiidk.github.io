b0bb9073e567227ad5ccf092aa8c70d43c2088f0
idiidk <idiidk@users.noreply.github.com>
2022-05-12 12:58:55 +0000

    deploy: 116e1829b99ca33ce2a23c716e79417007ffefcf

--- _nuxt/static/1652343966/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652343966",layout:"no-overflow",error:a,state:{pixiApp:a},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"97400076"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1652360332/work/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652360332",layout:"default",error:a,state:{pixiApp:a},serverRendered:true,routePath:"\u002Fwork",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"741e7d72"}}}}(null));
