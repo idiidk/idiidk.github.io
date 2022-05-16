272415e842a85b15fc3df5061f9f630b51d4ca73
idiidk <idiidk@users.noreply.github.com>
2022-05-16 12:18:08 +0000

    deploy: b11bd8bd26f85b13d439f7ff1625d6748889e5cf

---
+++ _nuxt/static/1652703484/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652703484",layout:"no-overflow",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ca0ae01d"}}}}(null,"\u002F"));
